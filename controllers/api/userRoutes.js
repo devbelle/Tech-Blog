77dab5a3167fb4beb291908b726f9b0642805e8c
Devin <dbelle722@gmail.com>
2024-02-29 21:05:33 -0500

    User Route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,2 +1,115 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
+
+
+//find all
+router.get('/', async (req, res) => {
+    try{
+    const userData = User.findAll({
+      attributes: { exclude: ['password'] }
+    });
+    res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+      
+  });
+
+
+//find one user
+router.get('/:id', async (req, res) => {
+    try{
+
+     const userData =  User.findOne({
+      attributes: { exclude: ['password'] },
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Post,
+          attributes: ['id', 'title', 'post_text', 'created_at']
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'created_at'],
+          include: {
+            model: Post,
+            attributes: ['title', 'post_id']
+          }
+        }
+      ]
+    });
+    if(!userData) {
+        res.status(404).json({ message: "Nothing found under this user id!"});
+        return;
+        }
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+  });
+
+//  users posts
+router.post('/', async (req, res) => {
+    try {
+      const userData = await User.create(req.body);
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+  
+        res.status(200).json(userData);
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+  //get one user
+
+
+  //login
+  router.post('/login', async (req, res) => {
+    try {
+      const userData = await User.findOne({ where: { email: req.body.email } });
+  
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      const validPassword = await userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.username = userData.username;
+        req.session.logged_in = true;
+        
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+  
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+  //logout
+  router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
