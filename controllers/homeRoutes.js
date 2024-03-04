c4d1169a85fbf421123b976876a338ce4415ba5e
Devin <dbelle722@gmail.com>
2024-03-04 18:46:18 -0500

    logout

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -89,4 +89,14 @@ router.get('/', async (req, res) => {
       
   });
 
+  router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+
   module.exports = router;
